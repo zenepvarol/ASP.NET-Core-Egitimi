5d7eb33d28562f6c3d34b7d57a4b6c3d018444fc
zenepvarol <128228755+zenepvarol@users.noreply.github.com>
2026-04-14 16:34:06 +0300

    Logging tamamlandı, Products API eklendi

--- ProductApp/Program.cs
+++ ProductApp/Program.cs
@@ -1,23 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
